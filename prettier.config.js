687c822eeec9372a6121b32293987a34c96ad0cb
Tuan Tran <mptnt1988@gmail.com>
2023-11-07 12:36:15 +0700

    Update

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://bit.dev/reference/prettier/prettier-config
+ */
+
+module.exports = {
+    printWidth: 120,
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+}
